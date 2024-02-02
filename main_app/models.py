669a6ea677f0831b0a20598ea0640f1178d1c7df
edcarcruz <edcarcruz@gmail.com>
2024-02-01 21:42:47 -0500

    added detail pages

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shoe(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    material = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
+from .models import Shoe
 
 # Add the following import
 from django.http import HttpResponse
-
-class Shoe:
-  def __init__(self, name, color, description, material, image):
-    self.name = name
-    self.color = color
-    self.description = description
-    self.material = material
-    self.image = image
-
-shoes = [
-  Shoe('Cortez', 'red', 'Classic Cortez', 'suede','https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/amnnjjrhg2krih3k0tdd/nike-classic-cortez-premium-red-orbit-white-black-release-date.jpg'),
-  Shoe('Dunk', 'blue', 'Classic Dunk', 'leather', 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/9f5d3ab4-98b4-44fe-9823-3b409603b520/dunk-low-retro-shoes-p6gmkm.png'),
-  Shoe('Chuck Taylor', 'black', 'Classic Converse Shoe', 'canvas', 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/tgttyejtnd29yz3nbr4p/converse-chuck-taylor-all-star-low-top-unisex-shoes-NLRAok.png'),
-  Shoe('Cortez', 'white', 'Classic Cortez', 'suede', 'https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/yi9yjnvjqfx3qkcuu6eb/nike-cortez-all-white.jpg'),
-]
 
 # Define the home view
 def home(request):
@@ -26,4 +12,9 @@ def about(request):
   return render(request, 'about.html')
 
 def shoes_index(request):
+  shoes = Shoe.objects.all()
   return render(request, 'shoes/index.html', { 'shoes': shoes })
+
+def shoes_detail(request, shoe_id):
+  shoe = Shoe.objects.get(id=shoe_id)
+  return render(request, 'shoes/detail.html', { 'shoe': shoe })
