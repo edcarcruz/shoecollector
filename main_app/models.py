7c057e94ae9cc940ccb916a20ccc43f83f6677cc
edcarcruz <edcarcruz@gmail.com>
2024-02-05 17:32:52 -0500

    added purchase date

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Shoe(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +10,16 @@ class Shoe(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'shoe_id': self.id})
+
+class Purchase(models.Model):
+    date = models.DateField('purchase date')
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.shoe.name} purchased on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Shoe
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Add the following import
 from django.http import HttpResponse
+from .forms import PurchaseForm
 
 # Define the home view
 def home(request):
@@ -17,4 +19,29 @@ def shoes_index(request):
 
 def shoes_detail(request, shoe_id):
   shoe = Shoe.objects.get(id=shoe_id)
-  return render(request, 'shoes/detail.html', { 'shoe': shoe })
+  purchase_form = PurchaseForm()
+  return render(request, 'shoes/detail.html', { 'shoe': shoe, 'purchase_form': purchase_form })
+
+def add_purchase(request, shoe_id):
+  # create the ModelForm using the data in request.POST
+  form = PurchaseForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_purchase = form.save(commit=False)
+    new_purchase.shoe_id = shoe_id
+    new_purchase.save()
+  return redirect('detail', shoe_id=shoe_id)
+
+class ShoeCreate(CreateView):
+  model = Shoe
+  fields = '__all__'
+  
+class ShoeUpdate(UpdateView):
+  model = Shoe
+  fields = '__all__'
+  
+class ShoeDelete(DeleteView):
+  model = Shoe
+  success_url = '/shoes/'
